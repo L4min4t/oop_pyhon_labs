9e23123b9bc347b88c08cb37a1389aabb8a8147d
L$min$t <vlasiukk.i.v@gmail.com>
2021-10-07 02:09:51 +0300

    second task done

--- lab2_part2/task2.py
+++ lab2_part2/task2.py
@@ -1,3 +1,53 @@
-# Create a class that performs statistical processing of a text file - counting characters, words, sentences, etc.
-# Determine the required attributes-data and attributes-methods in class for working with the text file.
+from string import ascii_letters, digits, punctuation   
 
+class Text_handler:
+    def __init__(self, file_name):
+        with open(file_name, 'r') as f:
+            contents = f.read()
+        f.close()
+        self.__contents = contents
+
+    def how_many_letters(self):
+        counter = 0
+        for i in self.__contents:
+            if i in ascii_letters:
+                counter += 1
+        return counter
+    
+    def how_many_digits(self):
+        counter = 0
+        for i in self.__contents:
+            if i in digits:
+                counter += 1
+        return counter
+
+    def how_many_words(self):
+        counter = 0
+        for i in range(0, len(self.__contents) - 1):
+            if self.__contents[i] in ascii_letters and self.__contents[i + 1] in punctuation.replace('-', ' '):
+                counter += 1
+        return counter
+
+    def how_many_sentences(self):
+        counter = 0
+        flag = False
+        for i in range(0, len(self.__contents)):
+            if self.__contents[i] in ascii_letters and not flag:
+                flag = True
+            if self.__contents[i] in '.?!' and flag:
+                flag = False
+                counter += 1
+        return counter
+     
+    def all_statistic(self):
+        return f'\bStatistic:\nletters = {self.how_many_letters()}\nwords = {self.how_many_words()}\ndigits = {self.how_many_digits()}\nsentences = {self.how_many_sentences()}'
+
+    def __str__(self) -> str:
+        return self.__contents
+    
+
+def main():
+    a = Text_handler('text2.txt')
+    print(a, '\n\n\n', a.all_statistic())
+
+main()
