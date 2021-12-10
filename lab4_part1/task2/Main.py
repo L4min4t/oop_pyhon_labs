a5778125f46eb3200739289a791b1422842d530f
L4min4t <vlasiukk.i.v@gmail.com>
2021-12-10 19:17:35 +0200

    1

--- lab4_part1/task2/Main.py
+++ lab4_part1/task2/Main.py
@@ -7,7 +7,7 @@ from Person import Person
 from Notebook import Notebook
 
 def main():
-    x = Person('bebra', 'Barbara', '+380961921937', '01.12.2000')
+    x = Person('123', '312', '+380961921937', '01.12.2000')
     x1 = Person('asd', 'dsa', '+380961921945', '02.11.2003')
     x2 = Person('asd123', 'dsa123', '+380961921999', '01.11.1900')
     y = Notebook(x, x1)
