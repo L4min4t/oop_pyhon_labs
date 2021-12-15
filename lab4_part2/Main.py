26414cef1d5efe2f3a43212a04d3068881d621c1
L4min4t <vlasiukk.i.v@gmail.com>
2021-12-16 00:22:09 +0200

    lab4part2-done

---
+++ lab4_part2/Main.py
@@ -0,0 +1,25 @@
+# A software academy teaches two types of courses: local courses that are held in some of the academy’s local labs
+# and offsite courses held in some other town outside of the academy’s headquarters. Each course has a name, a
+# teacher assigned to teach it and a course program (sequence of topics). Each teacher has a name and knows the
+# courses he or she teaches. Both courses and teachers could be printed in human-readable text form. All your courses
+# should implement ICourse. Teachers should implement ITeacher. Local and offsite courses should implement ILocalCourse
+# and IOffsiteCourse respectively. Courses and teachers should be created only through the ICourseFactory interface
+# implemented by a class named CourseFactory. Write a program that will form courses of software academy.
+
+from classes import Teacher, LocalCourse, OffsiteCourse, CoursesFactory
+
+
+def main():
+    x = Teacher('Boris', 'c++, java')
+    w = Teacher('Evgenij', 'php')
+    y = LocalCourse('C++ course', x, 'room #1', 'syntax and procedure programing')
+    z = OffsiteCourse('Java course', x, 'Kyiv', 'base of oop')
+    q = CoursesFactory("localhost", "lab4", "admin", "1111")
+    # q.pack(w)
+    # q.pack(x, y, z)
+    q.unpack()
+    for item in q.container:
+       print(type(item), '\t', item)
+
+if __name__ == '__main__':
+    main()
---
+++ lab4_part2/db_dump.sql
@@ -0,0 +1,3 @@
+create table "course" ("name" varchar, "teacher_name" varchar, "place" varchar, "program" varchar, "is_local" bool);
+
+create table "teacher" ("name" varchar, "courses" varchar);
